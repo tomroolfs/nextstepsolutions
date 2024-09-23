36edeb5b4283642bccc4bc8fbad6b6c17530af4a
maxhagen115 <maxhagen115@gmail.com>
2024-09-23 11:10:07 +0200

    made includes

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<html> 
-    <H1>Hello World 2</H1>
-</html>
+<?php
+session_start();
+
+// Controleer of de gebruiker is ingelogd
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    // Zo niet, omleiden naar de login pagina
+    header("Location: includes/login.inc.php");
+    exit;
+} else {
+    $loggedin = $_SESSION['loggedin'];
+}
+?>
